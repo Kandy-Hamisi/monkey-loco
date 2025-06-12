26bdabfccdffae54e0a34d470a36126ff3d4895a
Kandy-Hamisi <hkandy2000@gmail.com>
2025-06-12 20:48:27 +0300

    Finished AuthForm

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -4,12 +4,25 @@ import React from "react";
 import {
   DefaultValues,
   FieldValues,
+  Form,
+  Path,
   SubmitHandler,
   useForm,
   UseFormReturn,
 } from "react-hook-form";
 import { ZodType } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { FIELD_NAMES, FIELD_TYPES } from "../../constants";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 interface Props<T extends FieldValues> {
   schema: ZodType<T>;
@@ -43,6 +56,47 @@ const AuthForm = <T extends FieldValues>({
           ? "Welcome back to MonkeyLoco"
           : "Create Your MonkeyLoco Acount"}
       </h1>
+      <p className="text-light-100">
+        {isSignIn
+          ? "Access the vast collection of resources, and stay updated"
+          : "Please complete all the fields and upload a valid university ID to gain access to the library"}
+      </p>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="space-y-6 w-full"
+        >
+          {Object.keys(defaultValues).map((field) => (
+            <FormField
+              key={field}
+              control={form.control}
+              name={field as Path<T>}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="capitalize">
+                    {FIELD_NAMES[field.name as keyof typeof FIELD_NAMES]}
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      required
+                      placeholder="shadcn"
+                      type={FIELD_TYPES[field.name as keyof typeof FIELD_TYPES]}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+          <Button type="submit" className="form-btn">
+            {isSignIn ? "Sign In" : "Sign Up"}
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 };
